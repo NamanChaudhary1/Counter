19dcb97f2b78e062294bc4cf0115644d82577141
Naman Chaudhary <91721649+NamanChaudhary1@users.noreply.github.com>
2023-06-21 00:32:03 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Counter
+
+One can use this to count anything, comes with increasing, decreasing and resetting the count features.
---
+++ app.js
@@ -0,0 +1,18 @@
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+
+const reset = () =>{
+    count = 0;
+    value.textContent = count;
+}
+
+const decrease = () =>{
+    count--;
+    value.textContent = count;
+}
+
+const increase = () =>{
+    count++;
+    value.textContent = count;
+}
